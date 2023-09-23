22f734722237ca7ca2cb3f04aae3dcf7d3db01cd
srijan <srijandubey3105@gmail.com>
2023-09-23 12:27:46 +0530

    setting up AWS S3 bucket to allow pdf uploads from all origins

---
+++ src/lib/s3.ts
@@ -0,0 +1,40 @@
+import AWS from 'aws-sdk';
+
+export async function uploadToS3(file: File) {
+    try {
+        AWS.config.update({
+            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY
+        });
+        const s3 = new AWS.S3({
+            params: {
+                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME
+            },
+            region: "ap-south-1"
+        });
+        const file_key = 'uploads/' + Date.now().toString() + file.name.replace(" ", "-");
+
+        const params = {
+            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+            Key: file_key,
+            Body: file,
+        }
+
+        const upload = s3.putObject(params).on('httpUploadProgress', evt => {
+            console.log('upload progress', parseInt((evt.loaded * 100 / evt.total).toString()) + '%')
+        }).promise();
+
+        await upload.then(data => {
+            console.log('upload success', file_key)
+        })
+
+        return Promise.resolve({
+            //to save it to the database
+            file_key,
+            file_name: file.name
+        })
+
+    } catch (error) {
+
+    }
+}

c42c16f22a1c2cb8e99a0fa5e74ef696a66ef365
srijan <srijandubey3105@gmail.com>
2023-09-23 09:54:34 +0530

    setting up neon db and drizzle orm

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,6 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
     return <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        < SignIn />;
+        <SignIn />;
     </div>
 }   
---
+++ src/lib/db/index.ts
@@ -0,0 +1,9 @@
+import { neon, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+neonConfig.fetchConnectionCache = true;
+
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL not found ')
+
+const sql = neon(process.env.DATABASE_URL);
+
+export const db = drizzle(sql);

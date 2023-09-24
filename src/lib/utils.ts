1dc9ec536c001d5dc4da8c0116ec0f136b33a350
srijan <srijandubey3105@gmail.com>
2023-09-24 22:11:02 +0530

    minor bugs + converting the namespace to ascii for pinecone to work

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function convertToAscii(inputString: string) {
+
+  return inputString.replace(/[^\x00-\x7F]+/g, "");
 }

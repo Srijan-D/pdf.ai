7a7b4f3d44f98377d94bceb86715f8a1e4c62997
srijan <srijandubey3105@gmail.com>
2023-09-27 16:38:23 +0530

    testing stripe webhook callback on endpoint

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-    publicRoutes: ["/"],
+    publicRoutes: ["/", "/api/webhook"],
+    // for stripe webhooks, we need to disable csrf protection
 });
 
 export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };

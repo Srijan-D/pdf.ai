e9bd6325170807bd55fd68157076905cfae6fba0
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2023-10-08 01:23:11 +0530

    updating right bucket name minor bug

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -8,7 +8,7 @@ export async function downloadFromS3(file_key: string) {
         });
         const s3 = new AWS.S3({
             params: {
-                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME3333
+                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME
             },
             region: "ap-south-1"
         });

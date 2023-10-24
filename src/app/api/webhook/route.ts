006339671be187662cca73fa759de7091bb6e8a0
srijan <srijandubey3105@gmail.com>
2023-10-24 20:08:15 +0530

    use pnpm

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -2,57 +2,56 @@ import { db } from "@/lib/db";
 import { userSubscriptions } from "@/lib/db/schema";
 import { stripe } from "@/lib/stripe";
 import { eq } from "drizzle-orm";
-import { headers } from "next/headers";
 import { NextResponse, NextRequest } from "next/server";
 import Stripe from "stripe";
 export async function POST(req: NextRequest) {
-    const body = await req.text();
-    const signature = headers().get("Stripe-Signature") as string;
-    let event: Stripe.Event;
+  const body = await req.text();
+  const signature = req.headers.get("Stripe-Signature") as string;
+  let event: Stripe.Event;
 
-    try {
-        event = stripe.webhooks.constructEvent(
-            body,
-            signature,
-            process.env.STRIPE_WEBHOOK_SECRET as string
-        );
-    } catch (error) {
-        return new NextResponse("webhook error", { status: 400 });
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET as string
+    );
+  } catch (error) {
+    return new NextResponse("webhook error", { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  // new subscription created
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+    if (!session?.metadata?.userId) {
+      return new NextResponse("no userid", { status: 400 });
     }
+    await db.insert(userSubscriptions).values({
+      userId: session.metadata.userId,
+      stripeSubscriptionId: subscription.id,
+      stripeCustomerId: subscription.customer as string,
+      stripePriceId: subscription.items.data[0].price.id,
+      stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+    });
+  }
 
-    const session = event.data.object as Stripe.Checkout.Session;
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+    await db
+      .update(userSubscriptions)
+      .set({
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      })
+      .where(eq(userSubscriptions.stripeSubscriptionId, subscription.id));
+  }
 
-    // new subscription created
-    if (event.type === "checkout.session.completed") {
-        const subscription = await stripe.subscriptions.retrieve(
-            session.subscription as string
-        );
-        if (!session?.metadata?.userId) {
-            return new NextResponse("no userid", { status: 400 });
-        }
-        await db.insert(userSubscriptions).values({
-            userId: session.metadata.userId,
-            stripeSubscriptionId: subscription.id,
-            stripeCustomerId: subscription.customer as string,
-            stripePriceId: subscription.items.data[0].price.id,
-            stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
-        });
-    }
-
-    if (event.type === "invoice.payment_succeeded") {
-        const subscription = await stripe.subscriptions.retrieve(
-            session.subscription as string
-        );
-        await db
-            .update(userSubscriptions)
-            .set({
-                stripePriceId: subscription.items.data[0].price.id,
-                stripeCurrentPeriodEnd: new Date(
-                    subscription.current_period_end * 1000
-                ),
-            })
-            .where(eq(userSubscriptions.stripeSubscriptionId, subscription.id));
-    }
-
-    return new NextResponse(null, { status: 200 });
+  return new NextResponse(null, { status: 200 });
 }
